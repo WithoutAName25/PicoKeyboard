16a76660f8e3e5356b70e70505096cd9aae953ae
WithoutAName25 <withoutaname@withoutaname.eu>
2024-01-20 16:40:34 +0100

    test lcd

--- main.cpp
+++ main.cpp
@@ -1,4 +1,23 @@
+#include "LCDLibrary.h"
+
+LCDDirectGraphics lcd(
+        spi1,
+        11,
+        10,
+        12,
+        9,
+        8,
+        7,
+        52,
+        40,
+        135,
+        240
+);
 
 int main() {
+    lcd.init();
+    lcd.clear(0x0000);
+    lcd.fillRect(50, 10, 84, 20, 0xFFFF);
+    lcd.fillRect(50, 220, 84, 230, 0xFFFF);
     return 0;
 }
