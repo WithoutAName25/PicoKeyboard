4766abaadd9db9b0300e61f825f3b00f2b6f50e0
WithoutAName25 <withoutaname@withoutaname.eu>
2024-10-14 13:43:37 +0200

    Only send key changes to other device

--- src/keys/SecondaryKeyStateController.cpp
+++ src/keys/SecondaryKeyStateController.cpp
@@ -7,6 +7,8 @@ extern CommandController commandController;
 SecondaryKeyStateController::SecondaryKeyStateController(uint8_t numKeys) : KeyStateController(numKeys) {}
 
 void SecondaryKeyStateController::updateKeyState(uint8_t keyId, bool isPressed, absolute_time_t timestamp) {
+    if (getKeyState(keyId).isPressed != isPressed) {
+        commandController.send<KeyCommand>(isPressed, keyId);
+    }
     KeyStateController::updateKeyState(keyId, isPressed, timestamp);
-    commandController.send<KeyCommand>(isPressed, keyId);
 }
