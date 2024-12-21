626610fd53ec7d21d29ef1aeada9fe766a58d3de
WithoutAName25 <withoutaname@withoutaname.eu>
2024-12-21 15:20:51 +0100

    tmp

--- src/keys/action/TabHoldAction.cpp
+++ src/keys/action/TabHoldAction.cpp
@@ -3,8 +3,13 @@
 
 extern Scheduler scheduler;
 extern KeyStateController keyStateController;
+extern HIDKeyboard hidKeyboard;
 
 void TabHoldAction::execute(absolute_time_t timestamp) {
+    if (areKeysBlocked) {
+        hidKeyboard.unblockNewKeys(blockingReference);
+        areKeysBlocked = false;
+    }
     KeyState &currentState = keyStateController.getKeyState(activationKeyId);
     if (currentState.isPressed && currentState.totalPressCount == activationState.totalPressCount) {
         holdAction->execute(activationKeyId, &activationState, timestamp);
@@ -18,17 +23,45 @@ void TabHoldAction::onKeyStateChange(uint8_t keyId, KeyState &state, absolute_ti
             tabAction->execute(activationKeyId, &activationState, timestamp);
         }
         keyStateController.removeKeyStateListener(listenerReference);
+        if (task != nullptr) {
+            task->cancel();
+            task = nullptr;
+        }
+        if (areKeysBlocked) {
+            task = scheduler.addTask(this, timestamp + 10000);
+        }
     }
 }
+
+TabHoldAction::TabHoldAction(std::unique_ptr<IKeyAction> tabAction,
+                             std::unique_ptr<IKeyAction> holdAction,
+                             uint64_t tabTimeout,
+                             uint64_t holdTimeout,
+                             uint64_t tabHoldTimeout,
+                             bool blockOtherKeys)
+        : tabAction(std::move(tabAction)),
+          holdAction(std::move(holdAction)),
+          tabTimeout(tabTimeout),
+          holdTimeout(holdTimeout),
+          tabHoldTimeout(tabHoldTimeout),
+          blockOtherKeys(blockOtherKeys) {}
 
 void TabHoldAction::execute(uint8_t keyId, KeyState *state, absolute_time_t timestamp) {
     if (task != nullptr) {
         task->cancel();
         task = nullptr;
     }
+    if (areKeysBlocked) {
+        hidKeyboard.unblockNewKeys(blockingReference);
+        areKeysBlocked = false;
+    }
     if (timestamp < activationTimestamp + tabHoldTimeout) {
         tabAction->execute(keyId, state, timestamp);
     } else {
+        if (blockOtherKeys) {
+            blockingReference = hidKeyboard.blockNewKeys(timestamp);
+            areKeysBlocked = true;
+        }
         task = scheduler.addTask(this, timestamp + holdTimeout);
         listenerReference = keyStateController.addKeyStateListener(this);
     }
--- src/keys/action/TabHoldAction.h
+++ src/keys/action/TabHoldAction.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include "IKeyAction.h"
 #include "util.h"
+#include "usb.h"
 #include "PressReleaseAction.h"
 
 class TabHoldAction : public IKeyAction, private IExecutable, private IKeyStateListener {
@@ -12,6 +13,7 @@ private:
     uint64_t tabTimeout;
     uint64_t holdTimeout;
     uint64_t tabHoldTimeout;
+    bool blockOtherKeys;
 
     uint8_t activationKeyId = 0;
     KeyState activationState = {};
@@ -20,18 +22,28 @@ private:
     KeyStateListenerReference listenerReference;
     Task *task = nullptr;
 
+    KeyBlockingReference blockingReference;
+    bool areKeysBlocked = false;
+
     void execute(absolute_time_t timestamp) override;
 
     void onKeyStateChange(uint8_t keyId, KeyState &state, absolute_time_t timestamp) override;
 
+    TabHoldAction(std::unique_ptr<IKeyAction> tabAction,
+                  std::unique_ptr<IKeyAction> holdAction,
+                  uint64_t tabTimeout,
+                  uint64_t holdTimeout,
+                  uint64_t tabHoldTimeout,
+                  bool blockOtherKeys = false);
+
 public:
     template<typename T0, typename T1>
     TabHoldAction(T0 tabAction, T1 holdAction, uint64_t tabTimeout, uint64_t holdTimeout, uint64_t tabHoldTimeout)
-            : tabAction(std::make_unique<T0>(std::move(tabAction))),
-              holdAction(std::make_unique<T1>(std::move(holdAction))),
-              tabTimeout(tabTimeout),
-              holdTimeout(holdTimeout),
-              tabHoldTimeout(tabHoldTimeout) {}
+            : TabHoldAction(std::make_unique<T0>(std::move(tabAction)),
+                            std::make_unique<T1>(std::move(holdAction)),
+                            tabTimeout,
+                            holdTimeout,
+                            tabHoldTimeout) {}
 
     template<typename T0, typename T1>
     [[maybe_unused]] TabHoldAction(T0 tabAction, T1 holdAction, uint64_t timeout, uint64_t tabHoldTimeout)
@@ -40,6 +52,14 @@ public:
     template<typename T0, typename T1>
     [[maybe_unused]] TabHoldAction(T0 tabAction, T1 holdAction, uint64_t timeout)
             : TabHoldAction(tabAction, holdAction, timeout, timeout, timeout) {}
+
+    TabHoldAction(uint8_t keycode, Modifier modifier, uint64_t timeout, uint64_t tabHoldTimeout)
+            : TabHoldAction(std::make_unique<KeyAction>(keycode),
+                            std::make_unique<ModifierAction>(modifier),
+                            timeout,
+                            timeout,
+                            tabHoldTimeout,
+                            true) {}
 
     void execute(uint8_t keyId, KeyState *state, absolute_time_t timestamp) override;
 };
